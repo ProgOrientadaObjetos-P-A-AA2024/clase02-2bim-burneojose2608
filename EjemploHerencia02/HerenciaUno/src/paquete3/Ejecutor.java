1829c1d5b8c639d8dd9dcb4c6018660c76477465
jose <jgabriel2608@gmail>
2024-06-13 16:09:36 -0500

    trabajo

--- EjemploHerencia02/HerenciaUno/src/paquete3/Ejecutor.java
+++ EjemploHerencia02/HerenciaUno/src/paquete3/Ejecutor.java
@@ -14,17 +14,10 @@ public class Ejecutor {
         
         // Creación de un objeto de tipo EstudianteDistancia
         
-        String nombre = "René";
-        String apellido = "Elizalde";
-        String identificacion = "110011";
-        int edad = 36;
-        
-        EstudianteDistancia estDistancia = new EstudianteDistancia(nombre, 
-                apellido, identificacion, edad);
-        estDistancia.establecerNumeroAsginaturas(5);
-        estDistancia.establecerCostoAsignatura(100);
-        estDistancia.calcularMatriculaDistancia();
-        System.out.println(estDistancia);
+      
+      
+
+       
                 
         
     }
--- EjemploHerencia02/HerenciaUno/src/paquete3/EstudiantePresencial.java
+++ EjemploHerencia02/HerenciaUno/src/paquete3/EstudiantePresencial.java
@@ -43,5 +43,12 @@ public class EstudiantePresencial extends Estudiante{
     public double obtenerMatriculaPresencial(){
         return matriculaPresencial;
     }
-    
+     public String toString(){
+        String cadena = String.format(" Aepllido: %s\n Identificacion :%s\n "
+                + "Valor matricula presencial: %.2f"
+                , apellidosEstudiante,identificacionEstudiante,
+                matriculaPresencial);
+        
+        return cadena;
+    }
 }
